67315e1313238a4cd7a0d2825735879d19e07c6e
RYU <fbgusdnr9@naver.com>
2024-11-29 18:00:07 +0900

    GreenGramVer2

---
+++ src/main/java/green/greengramver2/GreenGramVer2Application.java
@@ -0,0 +1,13 @@
+package green.greengramver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenGramVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenGramVer2Application.class, args);
+    }
+
+}
